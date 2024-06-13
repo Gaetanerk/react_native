143b6388f24f6849d333e89405b8714ef2c71def
Gaetanerk <gaetan.turben@outlook.com>
2024-06-13 10:51:42 +0200

    exo4

--- src/components/Exos/Exo4/DetailContact.jsx
+++ src/components/Exos/Exo4/DetailContact.jsx
@@ -1,12 +1,10 @@
 import { Button, Linking, Text } from "react-native";
 
 const DetailContact = ({route}) => {
-        const id = route.params.id;
+    const id = route.params.id;
     const name = route.params.name;
     const firstname = route.params.firstname;
     const number = route.params.number;
-
-    console.log(parseInt(number.replaceAll('-', '')));
 
     function handleCallPress() {
         Linking.openURL('tel:' + number);
